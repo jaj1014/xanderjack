bc59a01f73c6c5a61e5b29f894e4dba8d1d93980
Alex <jaj1014@gmail.com>
2018-12-31 12:33:13 -0500

    fix an issue with last check commit

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 // Modules
-import page from './modules/page/'
+import page from './modules/pages/'
 import theme from './modules/theme/'
 
 Vue.use(Vuex)
