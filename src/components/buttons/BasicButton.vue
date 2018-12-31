e1fd6f0eff1d7074374c24cc839b11cba11362bb
Alex <jaj1014@gmail.com>
2018-12-31 13:01:53 -0500

    removed modal file

--- src/components/buttons/BasicButton.vue
+++ src/components/buttons/BasicButton.vue
@@ -1,10 +1,21 @@
 <template>
-  <button></button>
+  <button :style="color">Click me</button>
 </template>
 
 <script>
 export default {
-  
+  name: "basic-button",
+  props: {
+    title: String,
+    color: String
+  },
+  computed: {
+    color() {
+      return {
+        backgroundColor: this.color
+      }
+    }
+  }
 }
 </script>
 
