c2fb8e0892a247b0da77796acebaa464dfe6a70c
jaj1014 <ajax@pendo.io>
2019-07-08 08:37:59 -0400

    Working through adding a vue route for board-foot-calculator instead of static

--- src/main.js
+++ src/main.js
@@ -3,18 +3,33 @@ import 'typeface-montserrat/index.css'
 import './styles/main.scss'
 
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import store from './store'
 
 // Vendor js
 import VueTippy from 'vue-tippy'
 Vue.use(VueTippy)
 
-import App from './App.vue'
+// Component Imports
+import App from './App'
+import BoardFootCalculator from './views/BoardFootCalculator'
+
+// Routes
+const routes = [
+  { path: '/', component: App },
+  { path: '/board-foot-calculator', component: BoardFootCalculator }
+]
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
 
 Vue.config.productionTip = false
 
 new Vue({
   el: '#app',
   store,
+  router,
   render: h => h(App),
 })
