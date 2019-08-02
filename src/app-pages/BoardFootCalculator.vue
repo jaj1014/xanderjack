f87e313999293c37688ef8ab1020a8d432fba00b
jaj1014 <ajax@pendo.io>
2019-08-02 12:41:25 -0400

    updating call order for pendo snippet

--- src/app-pages/BoardFootCalculator.vue
+++ src/app-pages/BoardFootCalculator.vue
@@ -56,12 +56,14 @@ export default {
       return (this.boardWidth * this.actualLength * this.boardThickness) / 144;
     }
   },
-  beforeMount () {
-    const user = window.prompt('Please enter a user id', 'harry-potter')
-    const account = window.prompt('Please enter an account id', 'gryffindor')
-    const lang = window.prompt('Please enter a valid lang code', 'en-US')
+  beforeMount() {
+    this.loadPendo();
 
-    this.initPendo(user, account, lang)
+    const user = window.prompt("Please enter a user id", "harry-potter");
+    const account = window.prompt("Please enter an account id", "gryffindor");
+    const lang = window.prompt("Please enter a valid lang code", "en-US");
+
+    this.initPendo(user, account, lang);
   },
   methods: {
     initPendo(user, account, lang) {
@@ -73,12 +75,42 @@ export default {
         account: {
           id: account
         }
-      })
+      });
 
-      window.pendo.events.validateGuide(function onValidateGuide(hashableGuideString) {
-        console.log(hashableGuideString)
-        return false
-      })
+      window.pendo.events.validateGuide(function onValidateGuide(
+        hashableGuideString
+      ) {
+        console.log(hashableGuideString);
+        return false;
+      });
+    },
+    loadPendo() {
+      (function(apiKey) {
+        (function(p, e, n, d, o) {
+          var v, w, x, y, z;
+          o = p[d] = p[d] || {};
+          o._q = [];
+          v = ["initialize", "identify", "updateOptions", "pageLoad"];
+          for (w = 0, x = v.length; w < x; ++w)
+            (function(m) {
+              o[m] =
+                o[m] ||
+                function() {
+                  o._q[m === v[0] ? "unshift" : "push"](
+                    [m].concat([].slice.call(arguments, 0))
+                  );
+                };
+            })(v[w]);
+          y = e.createElement(n);
+          y.async = !0;
+          y.src =
+            "https://pendo-wildlings-static.storage.googleapis.com/agent/static/" +
+            apiKey +
+            "/pendo.js";
+          z = e.getElementsByTagName(n)[0];
+          z.parentNode.insertBefore(y, z);
+        })(window, document, "script", "pendo");
+      })("179c9d85-7d58-469c-7566-b166303c6e6f");
     }
   }
 };
