230ce23c9bf64864b2fda82ccf5a16ae368869a7
jaj1014 <ajax@pendo.io>
2019-08-02 12:51:28 -0400

    adding deploy

--- src/app-pages/BoardFootCalculator.vue
+++ src/app-pages/BoardFootCalculator.vue
@@ -57,8 +57,6 @@ export default {
     }
   },
   beforeMount() {
-    this.loadPendo();
-
     const user = window.prompt("Please enter a user id", "harry-potter");
     const account = window.prompt("Please enter an account id", "gryffindor");
     const lang = window.prompt("Please enter a valid lang code", "en-US");
@@ -83,34 +81,6 @@ export default {
         console.log(hashableGuideString);
         return false;
       });
-    },
-    loadPendo() {
-      (function(apiKey) {
-        (function(p, e, n, d, o) {
-          var v, w, x, y, z;
-          o = p[d] = p[d] || {};
-          o._q = [];
-          v = ["initialize", "identify", "updateOptions", "pageLoad"];
-          for (w = 0, x = v.length; w < x; ++w)
-            (function(m) {
-              o[m] =
-                o[m] ||
-                function() {
-                  o._q[m === v[0] ? "unshift" : "push"](
-                    [m].concat([].slice.call(arguments, 0))
-                  );
-                };
-            })(v[w]);
-          y = e.createElement(n);
-          y.async = !0;
-          y.src =
-            "https://pendo-wildlings-static.storage.googleapis.com/agent/static/" +
-            apiKey +
-            "/pendo.js";
-          z = e.getElementsByTagName(n)[0];
-          z.parentNode.insertBefore(y, z);
-        })(window, document, "script", "pendo");
-      })("179c9d85-7d58-469c-7566-b166303c6e6f");
     }
   }
 };
